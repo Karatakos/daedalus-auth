b7696087880da171b3d9e4012caeaed0114019cc
Karatakos <adrian.c.dunne@gmail.com>
2024-02-15 15:01:33 +0900

    Not just documentation

--- src/test/scripts/load-test-data.sh
+++ src/test/scripts/load-test-data.sh
@@ -1,9 +1,4 @@
 aws dynamodb batch-write-item \
-    --request-items file://dynamo-mock-artifacts.json \
-    --return-consumed-capacity INDEXES \
-    --return-item-collection-metrics SIZE
-
-aws dynamodb batch-write-item \
     --request-items file://dynamo-mock-users.json \
     --return-consumed-capacity INDEXES \
     --return-item-collection-metrics SIZE
